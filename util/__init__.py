c0ac2c424714d60bbaa140405a8f300d5bcd414f
Main <49382455+ShiroDoMain@users.noreply.github.com>
2021-02-07 20:30:16 +0800

    remove something

--- util/__init__.py
+++ util/__init__.py
@@ -2,9 +2,6 @@
 # @Time : 2020/7/29 3:55 下午
 # @Author : shiro
 # @Software: PyCharm
-from mirai import Mirai, Friend, Plain, Group, Image, At, Member, MessageChain, Source
-
-
 AcgImgPath = 'ATRIRoBot/AcgImg'
 
 
