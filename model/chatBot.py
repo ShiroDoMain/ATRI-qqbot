57353af26e464f623cb5588778ce5969d743ce15
ShiroDomain <www.1808107177@qq.com>
2021-10-05 10:04:45 +0800

    new api

--- model/chatBot.py
+++ model/chatBot.py
@@ -1,13 +1,12 @@
 import aiohttp
+
+
 class ChatBot(object):
-    BASE = 'http://nekomimi.icu/'
-    def __init__(self):
-        self.BASE = 'http://nekomimi.icu/'
+    BASE = 'http://api.nekomimi.icu/v1/'
 
     @classmethod
-    async def chat(cls,msg:str) -> dict:
-        async with aiohttp.request('GET',cls.BASE+'chatbot?msg=%s'%msg) as response:
+    async def chat(cls, msg: str) -> dict:
+        async with aiohttp.request('GET', cls.BASE + 'chat?msg=%s' % msg) as response:
             if response.status != 200 or (await response.json())['status'] != 'success':
-                return {'status':'bad'}
+                return {'status': 'bad'}
             return await response.json()
-
